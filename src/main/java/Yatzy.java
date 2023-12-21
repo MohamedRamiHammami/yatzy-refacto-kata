528bee6d9330d4b3e5d2dc7c056b5464ec1d93dc
MohamedRamiHammami <med.rami.hammami@gmail.com>
2023-12-21 14:31:11 +0100

    Refactor chance

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -5,13 +5,7 @@ public class Yatzy {
 	}
 
 	public static int chance(int d1, int d2, int d3, int d4, int d5) {
-		int total = 0;
-		total += d1;
-		total += d2;
-		total += d3;
-		total += d4;
-		total += d5;
-		return total;
+		return getSum(d1, d2, d3, d4, d5);
 	}
 
 	public static int yatzy(int d1, int d2, int d3, int d4, int d5) {
@@ -246,5 +240,9 @@ public class Yatzy {
 			return _2_at * 2 + _3_at * 3;
 		else
 			return 0;
+	}
+
+	private static int getSum(int d1, int d2, int d3, int d4, int d5) {
+		return d1 + d2 + d3 + d4 + d5;
 	}
 }
